4082ba628f131eccb2e8162894593ba9ae3648d0
jxmijuholx <jami.juhola01@gmail.com>
2024-02-07 11:06:04 +0100

    tehtävä valmis

--- App.js
+++ App.js
@@ -1,13 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, FlatList, Image, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [hakusana, setHakusana] = useState('')
+  const [meals, setMeals] = useState([])
+
+  const getRecipes = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${hakusana}`)
+    .then(response => response.json())
+    .then(responseJson => setMeals(responseJson.meals))
+    .catch(error =>{
+      Alert.alert('Error', error)
+    })
+  }
+
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <StatusBar hidden={true}/>
+      <FlatList
+        style={{marginLeft : "5%"}}
+        keyExtractor={(item, index) => index.toString()} 
+        renderItem={({item}) => 
+        <View>
+          <Text style={{fontSize: 18, fontWeight: "bold"}}>{item.strMeal}</Text>
+          <Image 
+            style={{width: 50, height: 50}} 
+            source={{uri: item.strMealThumb}} 
+          />
+        </View>}
+        data={meals}
+        ItemSeparatorComponent={listSeparator} 
+      />
+      <TextInput 
+          style={{fontSize: 18, width: 200}} 
+          placeholder='Search' 
+          value={hakusana}
+          onChangeText={text => setHakusana(text)} 
+        />
+        <Button title="Find" onPress={getRecipes} />
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
